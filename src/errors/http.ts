44e0dccea3fc3585038fc1e7ce8cbfa81c930288
Liberty Mugwara <libertymugwara@live.com>
2022-12-31 11:28:25 +0200

    feat: add internal http response errors

--- src/errors/http.ts
+++ src/errors/http.ts
@@ -1,20 +1,46 @@
 import { Response } from 'node-fetch';
 
-export class HTTPResponseError extends Error {
+export class ExternalHTTPResponseError extends Error {
   response: Response;
   statusCode: number;
-  constructor(response: Response) {
-  	super(`HTTP Error Response: ${response.status} ${response.statusText}`);
-  	this.response = response;
-  	this.statusCode = response.status;
+  constructor({ response, message }: { response: Response; message?: string }) {
+    super(
+      message ||
+        `HTTP Error Response: ${response.status} ${response.statusText}`,
+    );
+    this.response = response;
+    this.statusCode = response.status;
   }
 }
 
-export const checkStatus = (response: Response) => {
-	if (response.ok) {
-		// response.status >= 200 && response.status < 300
-		return response;
-	} else {
-		throw new HTTPResponseError(response);
-	}
+export class HTTPResponseError extends Error {
+  statusCode: number;
+  constructor({
+    statusCode,
+    message,
+  }: {
+    statusCode: number;
+    message: string;
+  }) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export const checkStatus = (response: Response, resourceName: string) => {
+  const statusMap = {
+    404: `The ${resourceName} was not found!`,
+    401: `You are not authenticated/authorized to access the ${resourceName}.`,
+    403: `You do not have the permissions to access the ${resourceName}.`,
+    500: `The server hosting the ${resourceName} has encountered an internal error.`,
+  };
+  if (response.ok) {
+    // response.status >= 200 && response.status < 300
+    return response;
+  } else {
+    throw new ExternalHTTPResponseError({
+      response,
+      message: statusMap[response.status],
+    });
+  }
 };
